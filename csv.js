034cae514ccfe820259e97df868592c7b1be2907
fiddleplum <stephenhurley@gmail.com>
2015-05-29 16:30:13 -0700

    Made error handling in csv better

--- csv.js
+++ csv.js
@@ -13,21 +13,21 @@ var csv =
 			var lineNum = 1;
 			while(i < text.length)
 			{
-				var o = csv._parseLine(text, i, lineNum, failFunction, url);
-				if(o == null)
+				var o = csv._parseLine(text, i, lineNum);
+				if(o.error != '')
 				{
+					failFunction(url, 'Error loading ' + filename + '. ' + o.error);
 					return null;
 				}
-				var fields = o[0];
-				console.log(fields);
-				i = o[1];
+				var fields = o.fields;
+				i = o.i;
 				if(i == text.length && fields.length == 0)
 				{
 					break;
 				}
 				if(data.length > 0 && fields.length != data[0].length)
 				{
-					failFunction(url, 'On line ' + lineNum + ', incorrect number of fields');
+					failFunction(url, 'Error loading ' + filename + '. On line ' + lineNum + ', there are an incorrect number of fields. ');
 					return null;
 				}
 				if(hasHeaders && data.length > 0)
@@ -52,7 +52,7 @@ var csv =
 			passFunction(url, data);
 		}, function(url, status)
 		{
-			failFunction(url, 'Failed to download: ' + status);
+			failFunction(url, 'Failed to download: ' + status + '. ');
 		});
 	},
 	
@@ -61,90 +61,89 @@ var csv =
 	// returns [array fields, int new_i]
 	_parseLine : function(text, i, lineNum, failFunction, url)
 	{
-		var fields = [];
+		var o = {};
+		o.fields = [];
+		o.i = i;
+		o.error = '';
 		var fieldNum = 1;
 		var field = '';
 		var quoted = false;
-		while(i < text.length)
+		while(o.i < text.length)
 		{
-			var c = text[i];
-			var nc = (i + 1 < text.length ? text[i + 1] : '');
-			console.log(c + " " + nc + " " + lineNum + " " + fieldNum + " " + quoted + " |" + field + "|");
+			var c = text[o.i];
+			var nc = (o.i + 1 < text.length ? text[o.i + 1] : '');
 			if(c == '"')
 			{
 				if(field == '') // start of quoted field
 				{
-					console.log('start quote');
 					quoted = true;
-					i++;
+					o.i++;
 				}
 				else if(quoted)
 				{
-					if(i + 1 >= text.length || nc == '\n' || nc == '\r' || nc == ',') // end of quoted field
+					if(o.i + 1 >= text.length || nc == '\n' || nc == '\r' || nc == ',') // end of quoted field
 					{
-						console.log('end quote');
-						fields.push(field);
+						o.fields.push(field);
 						field = '';
 						fieldNum++;
 						quoted = false;
-						i++; // pass quote
-						if(i + 1 >= text.length || nc == '\n' || nc == '\r') // end of line
+						o.i++; // pass quote
+						if(o.i + 1 >= text.length || nc == '\n' || nc == '\r') // end of line
 						{
-							while(i < text.length && (text[i] == '\n' || text[i] == '\r')) // get passed nl and cr
+							while(o.i < text.length && (text[o.i] == '\n' || text[o.i] == '\r')) // get passed nl and cr
 							{
-								i++;
+								o.i++;
 							}
-							return [fields, i];
+							return o;
 						}
 						else // pass comma
 						{
-							i++;
+							o.i++;
 						}
 					}
 					else if(nc == '"') // escaped quote
 					{
 						field += '"';
-						i++;
-						i++;
+						o.i += 2;
 					}
 					else // lonely double quote
 					{
-						failFunction(url, 'On line ' + lineNum + ', field ' + fieldNum + ', lonely double quote (escape with another quote)');
-						return null;
+						o.error = 'On line ' + lineNum + ', field ' + fieldNum + ', lonely double quote (escape with another quote). ';
+						return o;
 					}
 				}
 				else // double quote outside of double quoted field
 				{
-					failFunction(url, 'On line ' + lineNum + ', field ' + fieldNum + ', invalid double quote (quote the field and escape this one)');
-					return null;
+					o.error = 'On line ' + lineNum + ', field ' + fieldNum + ', invalid double quote (quote the field and escape this one). ';
+					return o;
 				}
 			}
 			else if(!quoted && (c == ',' || c == '\n' || c == '\r')) // end of field
 			{
-				fields.push(field);
+				o.fields.push(field);
 				field = '';
 				quoted = false;
 				fieldNum++;
 				if(c == '\n' || c == '\r') // end of line
 				{
-					while(i < text.length && (text[i] == '\n' || text[i] == '\r')) // get passed nl and cr
+					while(o.i < text.length && (text[o.i] == '\n' || text[o.i] == '\r')) // get passed nl and cr
 					{
-						i++;
+						o.i++;
 					}
-					return [fields, i];
+					return o;
 				}
 				else
 				{
-					i++;
+					o.i++;
 				}
 			}
 			else
 			{
 				field += c;
-				i++;
+				o.i++;
 			}
 		}
-		fields.push(field);
-		return [fields, i];
+		o.fields.push(field);
+		return o;
 	}
 };
