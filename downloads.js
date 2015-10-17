1088aa51373f1afc4a79c482831a37577a60fcc7
fiddleplum <stephenhurley@gmail.com>
2015-10-17 08:20:37 -0700

    Made downloads be able to dynamically load scripts

--- downloads.js
+++ downloads.js
@@ -1,52 +1,85 @@
 var downloads =
 {
 	// url - string
-	// passFunction - function (string url, string text)
-	// failFunction - function (string url, int status)
-	add: function (url, passFunction, failFunction)
+	// pass - function (string url, string text)
+	// fail - function (string url, int status)
+	add: function(url, pass, fail)
 	{
-		var download = {}
-		download.url = url;
-		download.passFunction = passFunction;
-		download.failFunction = failFunction;
+		var download = new this._Download(url, pass, fail, false);
+		this._queue.push(download);
+		this._update();
+	},
+	
+	// url - string
+	// pass - function (string url)
+	// fail - function (string url)
+	addScript: function(url, load, fail)
+	{
+		var download = new this._Download(url, load, fail, true);
 		this._queue.push(download);
 		this._update();
 	},
 
 	// Internal
 
+	_Download: function(url, pass, fail, script)
+	{
+		this.url = url;
+		this.pass = pass;
+		this.fail = fail;
+		this.script = script;
+	},
+
 	_active: [],
 	_queue: [],
 	_maxActive: 10,
+
 	_update: function()
 	{
 		if(this._active.length < this._maxActive && this._queue.length > 0)
 		{
 			var download = this._queue.shift();
-			var request = new XMLHttpRequest();
-			request.onreadystatechange = function ()
+			if(download.script)
 			{
-				if(request.readyState == 4)
+				var script = document.createElement('script');
+				script.src = download.url;
+				if(download.pass)
 				{
-					if(request.status == 200 || request.status == 0)
-					{
-						if(download.passFunction)
-						{
-							download.passFunction(download.url, request.responseText);
-						}
-					}
-					else
-					{
-						if(download.failFunction)
-						{
-							download.failFunction(download.url, request.status);
-						}
-					}
-					downloads._update();
+					script.onload = download.pass.bind(undefined, download.url);
 				}
+				if(download.fail)
+				{
+					script.onerror = download.fail.bind(undefined, download.url);
+				}
+				document.getElementsByTagName('head')[0].appendChild(script);
 			}
-			request.open("GET", download.url, true);
-			request.send();
+			else
+			{
+				var request = new XMLHttpRequest();
+				request.onreadystatechange = function()
+				{
+					if(request.readyState == 4)
+					{
+						if(request.status == 200 || request.status == 0)
+						{
+							if(download.pass)
+							{
+								download.pass(download.url, request.responseText);
+							}
+						}
+						else
+						{
+							if(download.fail)
+							{
+								download.fail(download.url, request.request.status);
+							}
+						}
+						downloads._update();
+					}
+				}
+				request.open("GET", download.url, true);
+				request.send();
+			}
 		}
 	}
 }
