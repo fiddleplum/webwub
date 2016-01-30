2fc05c44f174f70e822f75f305d7683e217e2ceb
fiddleplum <stephenhurley@gmail.com>
2016-01-29 16:17:30 -0800

    cleaned up downloader

--- downloader.js
+++ downloader.js
@@ -1,67 +1,61 @@
-var downloader = {
-	// url - string
-	// onDownload - function (string url, string text)
-	// onFail - function (string url, int status)
-	add: function(url, onDownload, onFail) {
-		var download = new this._Download(url, onDownload, onFail, false);
-		this._queue.push(download);
-		this._update();
-	},
-	
-	// url - string
-	// onLoad - function (string url)
-	// onFail - function (string url)
-	addScript: function(url, onLoad, onFail) {
-		var download = new this._Download(url, onLoad, onFail, true);
-		this._queue.push(download);
-		this._update();
-	},
+Downloader = {}
 
-	// Internal
+// url - string
+// onDownload - function (string url, string text)
+// onFail - function (string url, int status)
+Downloader.add = function(url, onDownload, onFail) {
+	var download = new this._Download(url, onDownload, onFail, false);
+	this._queue.push(download);
+	this._update();
+}
 
-	_Download: function(url, onDownload, onFail, script) {
-		this.url = url;
-		this.onDownload = onDownload;
-		this.onFail = onFail;
-		this.script = script;
-	},
+// url - string
+// onLoad - function (string url)
+// onFail - function (string url)
+Downloader.addScript = function(url, onLoad, onFail) {
+	var download = new this._Download(url, onLoad, onFail, true);
+	this._queue.push(download);
+	this._update();
+}
 
-	_update: function() {
-		if(this._active.length < this._maxActive && this._queue.length > 0) {
-			var download = this._queue.shift();
-			if(download.script) {
-				var script = document.createElement('script');
-				script.src = download.url;
-				if(download.onDownload) {
-					script.onload = download.onDownload.bind(undefined, download.url);
-				}
-				if(download.onFail) {
-					script.onerror = download.onFail.bind(undefined, download.url);
-				}
-				document.getElementsByTagName('head')[0].appendChild(script);
-			}
-			else {
-				var request = new XMLHttpRequest();
-				request.onreadystatechange = function() {
-					if(request.readyState == 4) {
-						if(request.status == 200 || request.status == 0) {
-							if(download.onDownload)
-								download.onDownload(download.url, request.responseText);
-						}
-						else {
-							if(download.onFail)
-								download.onFail(download.url, request.status);
-						}
-						downloader._update();
+// Internal
+
+Downloader._Download = function(url, onDownload, onFail, script) {
+	this.url = url;
+	this.onDownload = onDownload;
+	this.onFail = onFail;
+	this.script = script;
+},
+
+Downloader._update = function() {
+	if(this._active.length < this._maxActive && this._queue.length > 0) {
+		var download = this._queue.shift();
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if(request.readyState == 4) {
+				if(request.status == 200 || request.status == 0) {
+					if(download.script) {
+						eval(request.responseText);
+					}
+					if(download.onDownload) {
+						if(download.script)
+							download.onDownload(download.url);
+						else
+							download.onDownload(download.url, request.responseText);
 					}
 				}
-				request.open("GET", download.url, true);
-				request.send();
+				else {
+					if(download.onFail)
+						download.onFail(download.url, request.status);
+				}
+				Downloader._update();
 			}
 		}
-	},
-
-	_active: [],
-	_queue: [],
-	_maxActive: 10
+		request.open("GET", download.url, true);
+		request.send();
+	}
 }
+
+Downloader._active = [];
+Downloader._queue = [];
+Downloader._maxActive = 10;
